c33336794e4672dcf9f90b42a7cbfe74f35f6231
gyeongwonl <44036281+gyeongwonl@users.noreply.github.com>
2020-09-01 17:56:42 -0500

    finish ch1

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,2 +1,32 @@
-# Say hello
-print("Hello, World!")
+#!/usr/bin/env python
+"""
+Author: Gwon Lee
+Purpose: Say hello
+"""
+
+import argparse
+
+
+def get_args():
+    """ Get the command-line arguments """
+
+    parser = argparse.ArgumentParser(
+        description='Say hello',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-n', '--name', 
+                        metavar='name',
+                        default='World', 
+                        help='name to greet')
+    return parser.parse_args()
+
+
+def main():
+    """" main """
+
+    args = get_args()
+    name = args.name
+    print("Hello, " + name + "!")
+
+
+if __name__ == '__main__':
+    main()
